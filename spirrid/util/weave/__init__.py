3a80feff73d8a00148de1016348c9c93e25a4ae3
rostar <rrypl@.imb.rwth-aachen.de>
2012-03-05 15:26:21 +0100

    removed import

--- spirrid/util/weave/__init__.py
+++ spirrid/util/weave/__init__.py
@@ -6,7 +6,6 @@ from inline_tools import inline
 from numpy.testing import Tester
 from scipy.weave import converters
 from scipy.weave.ext_tools import ext_module, ext_function
-from scipy.weave.info import __doc__
 from scipy.weave.weave_version import weave_version as __version__
 import scipy.weave.ext_tools as ext_tools
 
