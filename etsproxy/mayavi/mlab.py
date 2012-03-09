e1e29e181d21b25bd1c1ad935d17181088e6dc17
Rostislav Chudoba <rostislav.chudoba@rwth-aachen.de>
2012-03-09 17:07:29 +0100

    etsproxy extended to fit ETS4

--- etsproxy/mayavi/mlab.py
+++ etsproxy/mayavi/mlab.py
@@ -1,7 +1,22 @@
-'''
-Created on Mar 9, 2012
+#-------------------------------------------------------------------------------
+#
+# Copyright (c) 2012
+# IMB, RWTH Aachen University,
+# ISM, Brno University of Technology
+# All rights reserved.
+#
+# This software is provided without warranty under the terms of the BSD
+# license included in the Spirrid top directory "licence.txt" and may be
+# redistributed only under the conditions described in the aforementioned
+# license.
+#
+# Thanks for using Simvisage open source!
+#
+#-------------------------------------------------------------------------------
 
-@author: rch
-'''
-
-from enthought.mayavi.mlab import *
+try:
+# ETS 3
+    from enthought.mayavi.mlab import *
+# ETS 4
+except ImportError:
+    from mayavi.mlab import *
