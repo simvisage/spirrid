cdeed5ad9774b1e4178275e2bef0c639a30fd0e9
rostar <rrypl@.imb.rwth-aachen.de>
2013-07-26 17:42:36 +0200

    new distr

--- spirrid/pdistrib/piecewise_uniform_distr.py
+++ spirrid/pdistrib/piecewise_uniform_distr.py
@@ -3,35 +3,37 @@ Created on 04.07.2013
 
 @author: acki
 '''
-'''
-Created on 20.06.2011
 
-@author: axel
-'''
-
-from scipy.stats.distributions import rv_continuous
-from numpy import pi, cos, sin, arccos
 from scipy.stats.distributions import rv_continuous
 import numpy as np
-class piecewise_uniform_gen( rv_continuous ):
-    import numpy as np
-    '''a and b , lower and upper area of distribution. ratio is weighting : a/(a+b)'''
-    def _pdf( self, x ):
-        a_lower, a_upper, b_lower, b_upper, ratio = 0.0, 0.03, 0.03, 0.95, 0.83
-        return 1 / ( a_upper - a_lower ) * ratio * ( x <= a_upper ) * ( x < b_lower ) * ( x >= a_lower ) + 1 / ( b_upper - b_lower ) * ( 1 - ratio )\
-        *( x > a_upper ) * ( x >= b_lower ) * ( x <= b_upper ) + ( x <= a_upper ) * ( x >= b_lower ) * ( 1 / ( a_upper - a_lower ) * ratio + 1 / ( b_upper - b_lower ) * ( 1 - ratio ) )
-        
-    def _cdf( self, x ):
-        a_lower, a_upper, b_lower, b_upper, ratio = 0.0, 0.03, 0.03, 0.95, 0.83
-        return ( x - a_lower ) / ( a_upper - a_lower ) * ratio * ( x <= a_upper ) * ( x < b_lower ) * ( x >= a_lower ) + ( x > a_upper ) * ( x < b_lower ) * ratio\
-            + ( ratio + ( x - b_lower ) / ( b_upper - b_lower ) * ( 1 - ratio ) ) * ( x >= b_lower ) * ( x <= b_upper ) * ( x > a_upper ) + ( x > b_upper )\
-            + ( x < b_upper ) * ( x <= a_upper ) * ( x >= b_lower ) * ( ( x - b_lower ) / ( b_upper - b_lower ) * ( 1 - ratio ) + ( x - a_lower ) / ( a_upper - a_lower ) * ratio )
-    def _ppf ( self, x ):
-        a_lower, a_upper, b_lower, b_upper, ratio = 0.0, 0.03, 0.03, 0.95, 0.83
-        return ( x <= ratio ) * ( a_lower + ( a_upper - a_lower ) * x / ratio )\
-            + ( x > ratio ) * ( b_lower + ( b_upper - b_lower ) * ( x - ratio ) / ( 1 - ratio ) )
 
-    def _stats( self ):
+
+class piecewise_uniform_gen(rv_continuous):
+
+    a_lower = 0.1
+    a_upper = .5
+    b_lower = .7
+    b_upper = 2.0
+    ratio = 0.8
+
+    '''a and b , lower and upper area of distribution. ratio is weighting : a/(a+b)'''
+    def _pdf(self, x):
+        return self.ratio / (self.a_upper - self.a_lower) * (x <= self.a_upper) * (x >= self.a_lower) \
+            + (1. - self.ratio) / (self.b_upper - self.b_lower) * (x >= self.b_lower) * (x <= self.b_upper)
+
+    def _cdf(self, x):
+        return self.ratio * (x - self.a_lower) / (self.a_upper - self.a_lower) \
+                * (x <= self.a_upper) * (x >= self.a_lower) \
+                + self.ratio * (x > self.a_upper) * (x < self.b_lower) \
+                + (self.ratio + (1. - self.ratio) * (x - self.b_lower) \
+                / (self.b_upper - self.b_lower)) * (x >= self.b_lower) * (x <= self.b_upper) \
+                + 1.0 * (x > self.b_upper)
+
+    def _ppf(self, x):
+        return (x <= self.ratio) * (self.a_lower + (self.a_upper - self.a_lower) * x / self.ratio)\
+            + (x > self.ratio) * (self.b_lower + (self.b_upper - self.b_lower) * (x - self.ratio) / (1 - self.ratio))
+
+    def _stats(self):
         return 0, 0, 0, 0
         '''
         return ( ( a_lower + a_upper ) * ratio + ( b_lower + b_upper ) * ( 1 - ratio ) ) / 2., \
@@ -41,17 +43,15 @@ class piecewise_uniform_gen( rv_continuous ):
              , 0.0, 0.0
              '''
 
-piecewise_uniform = piecewise_uniform_gen( a = 0.0, name = 'piecewise_uniform' )
+piecewise_uniform = piecewise_uniform_gen(a=0.0, name='piecewise_uniform')
 
 if __name__ == '__main__':
-    import numpy as np
     from matplotlib import pyplot as plt
-    
-    ini = piecewise_uniform
 
-    x_ppf = np.linspace( 0, 1, 1000 )
-    plt.plot( x_ppf, ini._ppf( x_ppf ) )
+    pu = piecewise_uniform
+
+    P = np.linspace(0.0001, .999, 300)
+    x = np.linspace(0.0, 2.0, 300)
+    plt.plot(P, pu._ppf(P))
+    plt.plot(pu._cdf(x), x, color='red', lw=2, ls='dashed')
     plt.show()
-
-            
-        
